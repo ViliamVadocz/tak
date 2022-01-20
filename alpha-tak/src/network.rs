1557361ed63dbbebbd6f8d9569e0b98f3ad06df4
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-01-20 13:45:58 +0100

    Implement batching

--- alpha-tak/src/network.rs
+++ alpha-tak/src/network.rs
@@ -2,6 +2,7 @@ use std::{error::Error, path::Path};
 
 use tak::game::Game;
 use tch::{
+    data::Iter2,
     nn,
     nn::{ConvConfig, ModuleT, OptimizerConfig},
     Device,
@@ -15,6 +16,8 @@ use crate::{
 };
 
 const EPOCHS: usize = 10; // TODO make bigger
+const BATCH_SIZE: i64 = 50; // TODO experiment
+const LEARNING_RATE: f64 = 1e-4;
 
 #[derive(Debug)]
 pub struct Network<const N: usize> {
@@ -30,8 +33,8 @@ pub struct Network<const N: usize> {
 impl<const N: usize> Network<N> {
     pub fn predict(&self, game: &Game<N>, train: bool) -> (Tensor, Tensor) {
         let input = game_repr(game);
-        let output = self.forward_t(&input, train);
-        let mut vec = output.split(moves_dims(N) as i64, 0);
+        let output = self.forward_t(&input.unsqueeze(0), train);
+        let mut vec = output.split(moves_dims(N) as i64, 1);
         let eval = vec.pop().unwrap();
         let policy = vec.pop().unwrap();
         (policy, eval)
@@ -39,18 +42,41 @@ impl<const N: usize> Network<N> {
 
     pub fn train(&mut self, examples: &[Example<N>]) {
         println!("starting training");
-        let mut opt = nn::Adam::default().build(&self.vs, 1e-4).unwrap();
-        // TODO somehow batch example together
-        for epoch in 0..EPOCHS {
-            println!("epoch: {}", epoch);
-            for Example { game, pi, v } in examples {
-                let (policy, eval) = self.predict(game, true);
+        let mut opt = nn::Adam::default().build(&self.vs, LEARNING_RATE).unwrap();
 
-                let loss_pi = -policy.dot(pi);
-                let loss_v = (eval - v).square();
+        // TODO consider symmetries
+        let games: Vec<_> = examples
+            .iter()
+            .map(|Example { game, .. }| game_repr(game))
+            .collect();
+        let targets: Vec<_> = examples
+            .iter()
+            .map(|Example { pi, v, .. }| Tensor::cat(&[pi, v], 0))
+            .collect();
+
+        for epoch in 0..EPOCHS {
+            // Batch examples
+            let mut batch_iter =
+                Iter2::new(&Tensor::stack(&games, 0), &Tensor::stack(&targets, 0), BATCH_SIZE);
+
+            println!("epoch: {}", epoch);
+            for (input, target) in batch_iter.shuffle() {
+                let batch_size = input.size()[0];
+                let output = self.forward_t(&input, true);
+                // get prediction
+                let mut vec = output.split(moves_dims(N) as i64, 1);
+                let eval = vec.pop().unwrap();
+                let policy = vec.pop().unwrap();
+
+                // Get target
+                let mut vec = target.split(moves_dims(N) as i64, 1);
+                let v = vec.pop().unwrap();
+                let pi = vec.pop().unwrap();
+
+                let loss_pi = -(pi * policy).sum(Kind::Float) / batch_size;
+                let loss_v = (eval - v).square().sum(Kind::Float) / batch_size;
                 let total_loss = loss_v + loss_pi;
 
-                // opt.zero_grad(); // TODO is this needed?
                 opt.backward_step(&total_loss);
             }
         }
@@ -104,20 +130,19 @@ impl<const N: usize> Default for Network<N> {
 impl<const N: usize> nn::ModuleT for Network<N> {
     fn forward_t(&self, input: &Tensor, train: bool) -> Tensor {
         let s = input
-            .unsqueeze(0)
             .apply(&self.conv1)
             .max_pool2d_default(2)
             .apply(&self.conv2)
             .max_pool2d_default(2)
             .apply(&self.conv3)
             .max_pool2d_default(2)
-            .reshape(&[2048])
+            .reshape(&[-1, 2048])
             .apply(&self.fc1)
             .relu()
             .dropout(0.5, train);
-        let policy = s.apply(&self.fc2).log_softmax(0, Kind::Float);
+        let policy = s.apply(&self.fc2).log_softmax(1, Kind::Float);
         let eval = s.apply(&self.fc3).tanh();
         // would be nice if I could just return two values
-        Tensor::cat(&[policy, eval], 0)
+        Tensor::cat(&[policy, eval], 1)
     }
 }
