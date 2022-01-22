61fa2a223b5b20b997273e2f74def7e3042a9986
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-01-22 12:59:14 +0100

    Increase self-play games

--- alpha-tak/src/self_play.rs
+++ alpha-tak/src/self_play.rs
@@ -13,9 +13,9 @@ use crate::{
     turn_map::LUT,
 };
 
-const SELF_PLAY_GAMES: u32 = 100;
+const SELF_PLAY_GAMES: u32 = 1000;
 const ROLLOUTS_PER_MOVE: u32 = 100;
-const PIT_GAMES: u32 = 50;
+const PIT_GAMES: u32 = 200;
 const WIN_RATE_THRESHOLD: f64 = 0.55;
 
 fn self_play<const N: usize>(network: &Network<N>) -> Vec<Example<N>>
