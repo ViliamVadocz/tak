c48a233666eda0511480bd1182096bbb598915e7
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-01-31 00:35:01 +0100

    Make network much smaller for experiment

--- alpha-tak/src/network.rs
+++ alpha-tak/src/network.rs
@@ -1,6 +1,5 @@
 use std::{error::Error, path::Path};
 
-use tak::game::Game;
 use tch::{
     data::Iter2,
     nn,
@@ -15,11 +14,10 @@ use crate::{
     repr::{game_repr, input_dims, moves_dims},
 };
 
-const EPOCHS: usize = 100;
-const BATCH_SIZE: i64 = 10_000;
-const LEARNING_RATE: f64 = 1e-3;
+const EPOCHS: usize = 10;
+const BATCH_SIZE: i64 = 1_000_000;
+const LEARNING_RATE: f64 = 1e-4;
 const WEIGHT_DECAY: f64 = 1e-4;
-const MIN_LOSS_DIFF: f32 = 0.2;
 
 #[derive(Debug)]
 pub struct Network<const N: usize> {
@@ -27,11 +25,8 @@ pub struct Network<const N: usize> {
     conv1: nn::Conv2D,
     conv2: nn::Conv2D,
     conv3: nn::Conv2D,
-    conv4: nn::Conv2D,
     fc1: nn::Linear,
     fc2: nn::Linear,
-    fc3: nn::Linear,
-    fc4: nn::Linear,
 }
 
 impl<const N: usize> Network<N> {
@@ -53,7 +48,6 @@ impl<const N: usize> Network<N> {
             .map(|Example { pi, v, .. }| Tensor::cat(&[pi, v], 0))
             .collect();
 
-        let mut last_loss = 0.0;
         for epoch in 0..EPOCHS {
             // Batch examples
             let mut batch_iter =
@@ -61,10 +55,8 @@ impl<const N: usize> Network<N> {
             let batch_iter = batch_iter
                 .to_device(Device::cuda_if_available())
                 .return_smaller_last_batch()
-                .shuffle(); // (Looks like shuffle has hardcoded CPU)
+                .shuffle();
 
-            println!("epoch: {}", epoch);
-            let mut epoch_loss = 0.;
             for (input, target) in batch_iter {
                 let batch_size = input.size()[0];
                 let output = self.forward_t(&input, true);
@@ -80,22 +72,11 @@ impl<const N: usize> Network<N> {
 
                 let loss_p = -(p * policy).sum(Kind::Float) / batch_size;
                 let loss_z = (z - eval).square().sum(Kind::Float) / batch_size;
+                println!("{epoch}: p={loss_p:.4?}\tz={loss_z:.4?}");
                 let total_loss = loss_z + loss_p;
 
                 opt.backward_step(&total_loss);
-                epoch_loss += {
-                    let x: f32 = total_loss.into();
-                    x
-                };
             }
-            println!("loss: {:?}", epoch_loss);
-
-            // quit early if loss diff is too small
-            let loss_diff = (last_loss - epoch_loss).abs();
-            if loss_diff < MIN_LOSS_DIFF {
-                break;
-            }
-            last_loss = epoch_loss;
         }
     }
 
@@ -117,60 +98,45 @@ impl<const N: usize> Default for Network<N> {
         let vs = nn::VarStore::new(Device::cuda_if_available());
         let root = &vs.root();
         let [d1, _d2, _d3] = input_dims(N);
-        let conv1 = nn::conv2d(root, d1 as i64, 32, 3, ConvConfig {
-            padding: 3,
+        let conv1 = nn::conv2d(root, d1 as i64, 128, 3, ConvConfig {
+            padding: 1,
             ..Default::default()
         });
-        let conv2 = nn::conv2d(root, 32, 64, 3, ConvConfig {
-            padding: 3,
+        let conv2 = nn::conv2d(root, 128, 128, 3, ConvConfig {
+            padding: 1,
             ..Default::default()
         });
-        let conv3 = nn::conv2d(root, 64, 128, 3, ConvConfig {
-            padding: 3,
+        let conv3 = nn::conv2d(root, 128, 128, 3, ConvConfig {
+            padding: 1,
             ..Default::default()
         });
-        let conv4 = nn::conv2d(root, 128, 128, 3, ConvConfig {
-            padding: 3,
-            ..Default::default()
-        });
-        let fc1 = nn::linear(root, (N * N * 128) as i64, 2048, Default::default());
-        let fc2 = nn::linear(root, 2048, 1024, Default::default());
-        let fc3 = nn::linear(root, 1024, moves_dims(N) as i64, Default::default());
-        let fc4 = nn::linear(root, 1024, 1, Default::default());
+        let fc1 = nn::linear(
+            root,
+            (N * N * 128) as i64,
+            moves_dims(N) as i64,
+            Default::default(),
+        );
+        let fc2 = nn::linear(root, (N * N * 128) as i64, 1, Default::default());
         Network {
             vs,
             conv1,
             conv2,
             conv3,
-            conv4,
             fc1,
             fc2,
-            fc3,
-            fc4,
         }
     }
 }
 
 impl<const N: usize> nn::ModuleT for Network<N> {
-    fn forward_t(&self, input: &Tensor, train: bool) -> Tensor {
+    fn forward_t(&self, input: &Tensor, _train: bool) -> Tensor {
         let s = input
             .apply(&self.conv1)
-            .max_pool2d_default(2)
             .apply(&self.conv2)
-            .max_pool2d_default(2)
             .apply(&self.conv3)
-            .max_pool2d_default(2)
-            .apply(&self.conv4)
-            .max_pool2d_default(2)
-            .reshape(&[-1, (N * N * 128) as i64])
-            .apply(&self.fc1)
-            .relu()
-            .dropout(0.5, train)
-            .apply(&self.fc2)
-            .relu()
-            .dropout(0.5, train);
-        let policy = s.apply(&self.fc3).log_softmax(1, Kind::Float);
-        let eval = s.apply(&self.fc4).tanh();
+            .reshape(&[-1, (N * N * 128) as i64]);
+        let policy = s.apply(&self.fc1).log_softmax(1, Kind::Float);
+        let eval = s.apply(&self.fc2).tanh();
         // would be nice if I could just return two values
         Tensor::cat(&[policy, eval], 1)
     }
