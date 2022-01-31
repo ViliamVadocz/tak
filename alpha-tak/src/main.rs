25c366f937034ce2012c35bba1ce00b920493054
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-01-31 16:05:34 +0100

    Save model with random name

--- alpha-tak/src/main.rs
+++ alpha-tak/src/main.rs
@@ -13,26 +13,23 @@ mod repr;
 mod self_play;
 mod turn_map;
 
-const START: usize = 0;
-
 fn main() {
     tch::maybe_init_cuda();
     println!("CUDA: {}", Cuda::is_available());
 
     let mut args = std::env::args();
     let mut nn = if let Some(model_path) = args.nth(1) {
-        Network::<4>::load(model_path).unwrap()
+        Network::<4>::load(&model_path).unwrap_or_else(|_| panic!("couldn't load model at {model_path}"))
     } else {
         println!("generating random model");
         Network::<4>::default()
     };
 
     let mut examples = Vec::new();
-    let mut i = START;
     loop {
         nn = play_until_better(nn, &mut examples);
         println!("saving model");
-        i += 1;
-        nn.save(format!("models/{i:03}.model")).unwrap();
+        let x = rand::random::<usize>() % 1_000_000;
+        nn.save(format!("models/{x:06}.model")).unwrap();
     }
 }
