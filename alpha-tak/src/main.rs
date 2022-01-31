827234ca80e9a45e0952f916a0cda17c382b0ecc
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-01-31 02:11:50 +0100

    Get model path as argument

--- alpha-tak/src/main.rs
+++ alpha-tak/src/main.rs
@@ -19,9 +19,9 @@ fn main() {
     tch::maybe_init_cuda();
     println!("CUDA: {}", Cuda::is_available());
 
-    let mut nn = if let Ok(nn) = Network::<4>::load(format!("models/{START:03}.model")) {
-        println!("using saved model");
-        nn
+    let mut args = std::env::args();
+    let mut nn = if let Some(model_path) = args.nth(1) {
+        Network::<4>::load(model_path).unwrap()
     } else {
         println!("generating random model");
         Network::<4>::default()
