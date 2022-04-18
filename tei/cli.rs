751531acb16de8b98ded476641d526bbf1243300
Viliam Vadocz <viliam.vadocz@gmail.com>
2022-04-18 16:56:20 +0200

    Move tei to seperate binary

---
+++ tei/cli.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+/// Train AlphaTak
+#[derive(Parser)]
+pub struct Args {
+    /// Path to model
+    pub model_path: String,
+    /// How many virtual rollouts to perform per batch
+    #[clap(short, long, default_value_t = 64)]
+    pub batch_size: u32,
+    /// Disable GPU usage
+    #[clap(short, long)]
+    pub no_gpu: bool,
+}
